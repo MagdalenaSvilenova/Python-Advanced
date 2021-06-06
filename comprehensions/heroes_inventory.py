ffed26035021404f16d86b91d75004d90f69a01b
MagdalenaSvilenova <m_svilenova@abv.bg>
2021-06-06 22:25:37 +0300

    Add files via upload

---
+++ comprehensions/heroes_inventory.py
@@ -0,0 +1,13 @@
+names = input().split(', ')
+heroes = {hero: {} for hero in names}
+
+command = input()
+
+while not command == 'End':
+    name, item, cost = command.split('-')
+    if name in heroes:
+        if item not in heroes[name]:
+            heroes[name][item] = int(cost)
+    command = input()
+
+[print(f"{hero} -> Items: {len(heroes[hero])}, Cost: {sum(heroes[hero].values())}") for hero in heroes]
